9d778d32d67c249516111f61383276d3da02f4d2
Gonaco <danielmoremanza@gmail.com>
2018-04-12 18:23:37 +0200

    Multiple qubits error detected

--- OpenQL_Translator/openql_translator.py
+++ OpenQL_Translator/openql_translator.py
@@ -53,14 +53,15 @@ def qlib2openql(stranger_file, dictionary, gates_buffer, lines):
                         gates_buffer.append("k.gate('" +
                                             dictionary[match[0][0]] + "'," +
                                             str(qubits_dict.index(match[0][1]))
-                                            + "," +
+                                            + "," if mult_qubits_trans else "" +
                                             ",".join(mult_qubits_trans)+")\n")
 
                         print("k.gate('" +
                               dictionary[match[0][0]] + "'," +
                               str(qubits_dict.index(match[0][1]))
-                              +
+                              + "," if mult_qubits_trans else "" +
                               ",".join(mult_qubits_trans)+")\n")
+
                 except IOError as err:
                     print("I/O error: {0}".format(err))
                 except ValueError:
