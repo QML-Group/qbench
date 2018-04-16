3cdf4669e99af50d084fe591daf9f9a22b49b2c3
leon <lriesebos@fastmail.com>
2018-04-16 19:43:51 +0200

    added aqft and scaffcc sqrt port. updated readme.

---
+++ benchmarks/quapa/openql_aqft.py
@@ -0,0 +1,110 @@
+import argparse
+import os
+import random
+import itertools
+
+import openql.openql as ql
+
+from qbt.util import Printer
+
+
+def controlled_phase(kernel, c, t, k):
+    assert isinstance(kernel, ql.Kernel)
+    assert isinstance(k, int) and k >= 1
+
+    if k == 1:
+        kernel.cphase(c, t)
+    else:
+        raise ValueError('Accurate AQFT implementation not supported at this moment')
+
+
+def main():
+    # Create printer
+    printer = Printer('openql_aqft')
+
+    try:
+        # Parse arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('config', type=str, help='OpenQL config file')
+        parser.add_argument('qubits', type=int, help='number of qubits')
+        parser.add_argument('a', type=int, help='approximation threshold, 1 or larger for better approximation')
+        parser.add_argument('-o', '--output', type=str, default='openql_output', help='output directory')
+        parser.add_argument('--swap', action='store_true', help='swap registers after applying AQFT')
+        parser.add_argument('--no-skip', action='store_true', help='do not skip gates, but insert simpler variants')
+        parser.add_argument('--no-prepare', action='store_true', help='do not prepare all qubits (PrepZ)')
+        parser.add_argument('--no-measure', action='store_true', help='do not measure all qubits (MeasZ)')
+        parser.add_argument('--scheduler', type=str, default='ALAP', help='OpenQL scheduling strategy')
+        args = parser.parse_args()
+
+        # Check arguments
+        if args.qubits <= 0:
+            raise ValueError('Qubits argument should be larger then 0')
+        if args.a <= 0:
+            raise ValueError('Approximation threshold should be larger than 0')
+
+        # Ensure output directory and set as OpenQL output directory
+        args.output = os.path.expanduser(args.output)
+        os.makedirs(args.output, mode=0o755, exist_ok=True)
+        ql.set_output_dir(args.output)
+
+        # Create OpenQL platform
+        printer.write('Initializing OpenQL platform with configuration %s ...' % args.config)
+        platform = ql.Platform('platform', os.path.expanduser(args.config))
+
+        # Set up OpenQL program and kernel
+        printer.write('Initializing OpenQL program...')
+        program = ql.Program('qft_%i' % args.qubits, args.qubits, platform)
+        kernel = ql.Kernel('aqft_kernel', platform)
+
+        # Define array of qubit indices
+        qubits = list(range(args.qubits))
+
+        if not args.no_prepare:
+            # Prepare
+            for q in qubits:
+                kernel.gate('prepz', [q])
+
+        # AQFT circuit
+        for i in range(len(qubits)):
+            kernel.hadamard(qubits[i])
+            for j in range(i + 1, len(qubits)):
+                k = j - i
+                if args.no_skip:
+                    # Cap the value of k to prevent skipping of gates
+                    k = min(k, args.a)
+                if k <= args.a:
+                    controlled_phase(kernel, qubits[j], qubits[i], k)
+
+        if args.swap:
+            # SWAP qubits
+            for i in range(len(qubits) // 2):
+                q0 = qubits[i]
+                q1 = qubits[-i - 1]
+                kernel.cnot(q0, q1)
+                kernel.cnot(q1, q0)
+                kernel.cnot(q0, q1)
+
+        if not args.no_measure:
+            # Measure
+            for q in qubits:
+                kernel.gate('measz', [q])
+
+        # Add kernel to program
+        program.add_kernel(kernel)
+
+        # Compile
+        printer.write('Compiling using OpenQL...')
+        program.compile(scheduler=args.scheduler)
+
+    except (ValueError, TypeError, FileNotFoundError) as e:
+        # Catch and print some exceptions
+        printer.write(e)
+        exit(1)
+    else:
+        # Print final message
+        printer.write('Done!')
+        exit(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ benchmarks/quapa/openql_scaffcc_sqrt.py
@@ -0,0 +1,191 @@
+import argparse
+import os
+import random
+import itertools
+import math
+
+import openql.openql as ql
+
+from qbt.util import Printer
+from qbt.helpers import NoneType
+
+
+def toffoli(kernel, c0, c1, t):
+    assert isinstance(kernel, ql.Kernel)
+    assert isinstance(c0, int)
+    assert isinstance(c1, int)
+    assert isinstance(t, int)
+
+    kernel.hadamard(t)
+    kernel.cnot(c1, t)
+    kernel.tdag(t)
+    kernel.cnot(c0, t)
+    kernel.t(t)
+    kernel.cnot(c1, t)
+    kernel.tdag(t)
+    kernel.cnot(c0, t)
+    kernel.tdag(c1)
+    kernel.t(t)
+    kernel.cnot(c0, c1)
+    kernel.hadamard(t)
+    kernel.tdag(c1)
+    kernel.cnot(c0, c1)
+    kernel.t(c0)
+    kernel.s(c1)
+
+
+def diffuse(kernel, qubits, scratch):
+    assert isinstance(kernel, ql.Kernel)
+    assert isinstance(qubits, list)
+    assert isinstance(scratch, list)
+    assert len(qubits) == len(scratch) + 1
+
+    for q in qubits:
+        kernel.hadamard(q)
+        kernel.x(q)
+
+    for q in scratch:
+        kernel.prepz(q)
+
+    toffoli_targets = list(zip(scratch[1:], scratch, qubits[2:]))
+    toffoli(kernel, scratch[0], qubits[1], qubits[0])
+    for x, y, q in toffoli_targets:
+        toffoli(kernel, x, y, q)
+
+    kernel.z(scratch[-1])
+
+    for x, y, q in reversed(toffoli_targets):
+        toffoli(kernel, x, y, q)
+    toffoli(kernel, scratch[0], qubits[1], qubits[0])
+
+    for q in qubits:
+        kernel.x(q)
+        kernel.hadamard(q)
+
+
+def eqxmark(kernel, qubits, scratch, t, tf):
+    assert isinstance(kernel, ql.Kernel)
+    assert isinstance(qubits, list)
+    assert isinstance(scratch, list)
+    assert len(qubits) == len(scratch) + 1
+    assert isinstance(t, int)
+    assert isinstance(tf, bool)
+
+    for i in range(len(qubits)):
+        if i != 1:
+            kernel.x(qubits[i])
+
+    for q in scratch:
+        kernel.prepz(q)
+
+    toffoli_targets = list(zip(scratch[1:], scratch, qubits[2:]))
+    toffoli(kernel, scratch[0], qubits[1], qubits[0])
+    for x, y, q in toffoli_targets:
+        toffoli(kernel, x, y, q)
+
+    if tf:
+        kernel.cnot(t, scratch[-1])
+    else:
+        kernel.z(scratch[-1])
+
+    for x, y, q in reversed(toffoli_targets):
+        toffoli(kernel, x, y, q)
+    toffoli(kernel, scratch[0], qubits[1], qubits[0])
+
+    for i in range(len(qubits)):
+        if i != 1:
+            kernel.x(qubits[i])
+
+
+def sqr(kernel, a, b):
+    assert isinstance(kernel, ql.Kernel)
+    assert isinstance(a, list)
+    assert isinstance(b, list)
+    assert len(a) == len(b)
+
+    for i in range(((len(a) - 1) // 2) + 1):
+        kernel.cnot(b[i * 2], a[i])
+
+    for i in range((len(a) + 1) // 2, len(a)):
+        k = (i * 2) - len(a)
+        kernel.cnot(b[k], a[i])
+        kernel.cnot(b[k + 1], a[i])
+
+
+def main():
+    # Create printer
+    printer = Printer('openql_scaffcc_sqrt')
+
+    try:
+        # Parse arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('config', type=str, help='OpenQL config file')
+        parser.add_argument('n', type=int, help='problem size (log of database size)')
+        parser.add_argument('-o', '--output', type=str, default='openql_output', help='output directory')
+        parser.add_argument('--scheduler', type=str, default='ALAP', help='OpenQL scheduling strategy')
+        args = parser.parse_args()
+
+        # Check arguments
+        if args.n <= 1:
+            raise ValueError('n should be 2 or larger')
+
+        # Ensure output directory and set as OpenQL output directory
+        args.output = os.path.expanduser(args.output)
+        os.makedirs(args.output, mode=0o755, exist_ok=True)
+        ql.set_output_dir(args.output)
+
+        # Create OpenQL platform
+        printer.write('Initializing OpenQL platform with configuration %s ...' % args.config)
+        platform = ql.Platform('platform', os.path.expanduser(args.config))
+
+        # Define array of qubit indices which can be reused
+        qubits = list(range(args.n * 3 - 1))
+
+        # Set up OpenQL program and kernel
+        printer.write('Initializing OpenQL program...')
+        program = ql.Program('sqrt_n%i' % args.n, len(qubits), platform)
+        kernel = ql.Kernel('scaffcc_sqrt_kernel', platform)
+
+        # Sqrt circuit
+        a = qubits[:args.n]
+        b = qubits[args.n:args.n * 2]
+        scratch = qubits[args.n * 2:args.n * 3 - 1]
+        t = qubits[-1]
+
+        for q in a:
+            kernel.prepz(q)
+            kernel.hadamard(q)
+
+        nstep = int(math.floor((math.pi / 4) * math.sqrt(args.n ** 2)))
+        for _ in range(nstep):
+            sqr(kernel, a, b)
+            eqxmark(kernel, b, scratch, t, False)
+            sqr(kernel, a, b)
+            diffuse(kernel, a, scratch)
+
+        sqr(kernel, a, b)
+        eqxmark(kernel, b, scratch, t, True)
+
+        kernel.measure(t)
+        for q in a:
+            kernel.measure(q)
+
+        # Add kernel to program
+        program.add_kernel(kernel)
+
+        # Compile
+        printer.write('Compiling using OpenQL...')
+        program.compile(scheduler=args.scheduler)
+
+    except (ValueError, TypeError, FileNotFoundError) as e:
+        # Catch and print some exceptions
+        printer.write(e)
+        exit(1)
+    else:
+        # Print final message
+        printer.write('Done!')
+        exit(0)
+
+
+if __name__ == '__main__':
+    main()
