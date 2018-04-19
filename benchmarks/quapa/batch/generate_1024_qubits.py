349295cdfe9b3ad66be0ea0ea8a65d0693cae3fe
leon <lriesebos@fastmail.com>
2018-04-19 15:33:04 +0200

    gate list became an append argument. therefore it is optional right now, but can be used multiple times.

--- benchmarks/quapa/batch/generate_1024_qubits.py
+++ benchmarks/quapa/batch/generate_1024_qubits.py
@@ -9,7 +9,7 @@ def main():
     # Parse arguments
     parser = ArgumentParser()
     parser.add_argument('config', type=str, help='OpenQL config file')
-    parser.add_argument('gate_list', type=str, help='CSV file with gate distributions')
+    parser.add_argument('-g', '--gate-list', type=str, action='append', help='CSV file with gate distributions')
     parser.add_argument('--output-dir', type=str, default='openql_output', help='output directory')
     args = parser.parse_args()
 
@@ -22,17 +22,19 @@ def main():
     # List of generators
     gen = list()
 
-    # AQFT
-    # gen.append(['../openql_aqft.py', q, 15] for q in qubits)
-
-    # Random
-    gen.append(['../openql_random_generator.py', args.gate_list, q, 256, '--reverse', '--normalize'] for q in qubits)
-
     # RB
     gen.append(['../openql_randomized_benchmarking.py', q, 128] for q in qubits)
 
+    # AQFT
+    # gen.append(['../openql_aqft.py', q, 15] for q in qubits)
+
     # Sqrt
     # gen.append(['../openql_scaffcc_sqrt.py', q // 3] for q in qubits)
+
+    if args.gate_list:
+        # Random
+        gen.append(['../openql_random_generator.py', g, q, 256, '--reverse', '--normalize']
+                   for q in qubits for g in args.gate_list)
 
     # Put all tasks
     for g in gen:
