fe33c1271a1e2fec55ea8d55b52c70cd7c0cd026
Gonaco <danielmoremanza@gmail.com>
2018-06-13 16:47:06 +0200

    Code to compile all the openql files at once selecting the common config_file and scheduler type

--- benchmarks/compile_all_openql.py
+++ benchmarks/compile_all_openql.py
@@ -6,7 +6,7 @@ def compile_all_openql(directory_path, config_file_path, scheduler="ASAP"):
 
     for filename in os.listdir(directory_path):
         print(filename)
-        if ".qasm" in filename:
+        if ".py" in filename:
 
             openql_comp = __import__(filename)
             openql_comp.circuit(config_file_path)
@@ -20,6 +20,13 @@ if __name__ == "__main__":
     parser.add_argument('openql_benchmark_directory',
                         help='Directory with all the OpenQL benchmarks to compile')
 
+    parser.add_argument('config_file_path',
+                        help='Path to the configuration file')
+
+    parser.add_argument('scheduler', nargs='?', default='ASAP',
+                        help='Scheduler kind (ASAP, ALAP, ...)')
+
     args = parser.parse_args()
 
-    compile_all_openql(args.openql_benchmark_directory)
+    compile_all_openql(args.openql_benchmark_directory,
+                       args.config_file_path, args.scheduler)
