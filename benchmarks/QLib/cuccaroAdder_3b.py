b9a862d57c5ccf39b9439dfd6653fe9400e21a7d
Gonaco <danielmoremanza@gmail.com>
2018-06-20 13:51:29 +0200

    Adding corrected translations and new benchmarks from QLib translated by Lingling

--- benchmarks/QLib/cuccaroAdder_3b.py
+++ benchmarks/QLib/cuccaroAdder_3b.py
@@ -1,35 +1,43 @@
 from openql import openql as ql
 import os
 import numpy as np
-curdir = os.path.dirname(__file__)
-output_dir = os.path.join(curdir, 'test_output')
-ql.set_output_dir(output_dir)
-config_fn = os.path.join(curdir, '/home/daniel/Master/Quantum_Computing_and_Quantum_Information/OpenQL/tests/hardware_config_cc_light.json')
-platform  = ql.Platform('platform_none', config_fn)
-sweep_points = [1,2]
-num_circuits = 1
-num_qubits = 9
-p = ql.Program('cuccaroAdder_3b', num_qubits, platform)
-p.set_sweep_points(sweep_points, num_circuits)
-k = ql.Kernel('cuccaroAdder_3b', platform)
-k.gate('cnot',1,2)
-k.gate('cnot',1,0)
-k.gate('toffoli',0,2,1)
-k.gate('cnot',3,4)
-k.gate('cnot',3,1)
-k.gate('toffoli',1,4,3)
-k.gate('cnot',5,6)
-k.gate('cnot',5,3)
-k.gate('toffoli',3,6,5)
-k.gate('cnot',5,7)
-k.gate('toffoli',3,6,5)
-k.gate('cnot',5,3)
-k.gate('cnot',3,6)
-k.gate('toffoli',1,4,3)
-k.gate('cnot',3,1)
-k.gate('cnot',1,4)
-k.gate('toffoli',0,2,1)
-k.gate('cnot',1,0)
-k.gate('cnot',0,2)
-p.add_kernel(k)
-p.compile(optimize=False)
+
+def circuit(config_file, scheduler='ASAP'):    curdir = os.path.dirname(__file__)
+    output_dir = os.path.join(curdir, 'test_output')
+    ql.set_option('output_dir', output_dir)
+    ql.set_option('optimize', 'no')
+    ql.set_option('scheduler', 'scheduler')
+    ql.set_option('log_level', 'LOG_WARNING')
+
+    config_fn = os.path.join(curdir, config_file)
+
+    platform  = ql.Platform('platform_none', config_fn)
+    sweep_points = [1,2]
+    num_circuits = 1
+    num_qubits = 9
+    p = ql.Program('cuccaroAdder_3b', num_qubits, platform)
+    p.set_sweep_points(sweep_points, num_circuits)
+    k = ql.Kernel('cuccaroAdder_3b', platform)
+    k.gate('cnot',[1,2])
+    k.gate('cnot',[1,0])
+    k.gate('toffoli',[0,2,1])
+    k.gate('cnot',[3,4])
+    k.gate('cnot',[3,1])
+    k.gate('toffoli',[1,4,3])
+    k.gate('cnot',[5,6])
+    k.gate('cnot',[5,3])
+    k.gate('toffoli',[3,6,5])
+    k.gate('cnot',[5,7])
+    k.gate('toffoli',[3,6,5])
+    k.gate('cnot',[5,3])
+    k.gate('cnot',[3,6])
+    k.gate('toffoli',[1,4,3])
+    k.gate('cnot',[3,1])
+    k.gate('cnot',[1,4])
+    k.gate('toffoli',[0,2,1])
+    k.gate('cnot',[1,0])
+    k.gate('cnot',[0,2])
+
+    p.add_kernel(k)
+    p.compile()
+
