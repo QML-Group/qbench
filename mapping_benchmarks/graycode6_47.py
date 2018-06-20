d4482a72604da97c24cb3c108c70a4a3525edb7a
Gonaco <danielmoremanza@gmail.com>
2018-06-20 14:33:36 +0200

    Updateing the mapping bechmark list with also the RevLib ones

--- mapping_benchmarks/graycode6_47.py
+++ mapping_benchmarks/graycode6_47.py
@@ -1,21 +1,29 @@
 from openql import openql as ql
 import os
 import numpy as np
-curdir = os.path.dirname(__file__)
-output_dir = os.path.join(curdir, 'test_output')
-ql.set_output_dir(output_dir)
-config_fn = os.path.join(curdir, '/home/daniel/Master/Quantum_Computing_and_Quantum_Information/OpenQL/tests/hardware_config_cc_light.json')
-platform  = ql.Platform('platform_none', config_fn)
-sweep_points = [1,2]
-num_circuits = 1
-num_qubits = 6
-p = ql.Program('graycode6_47', num_qubits, platform)
-p.set_sweep_points(sweep_points, num_circuits)
-k = ql.Kernel('graycode6_47', platform)
-k.gate('cnot',1,0)
-k.gate('cnot',2,1)
-k.gate('cnot',3,2)
-k.gate('cnot',4,3)
-k.gate('cnot',5,4)
-p.add_kernel(k)
-p.compile(optimize=False)
+
+def circuit(config_file, scheduler='ASAP'):    curdir = os.path.dirname(__file__)
+    output_dir = os.path.join(curdir, 'test_output')
+    ql.set_option('output_dir', output_dir)
+    ql.set_option('optimize', 'no')
+    ql.set_option('scheduler', 'scheduler')
+    ql.set_option('log_level', 'LOG_WARNING')
+
+    config_fn = os.path.join(curdir, config_file)
+
+    platform  = ql.Platform('platform_none', config_fn)
+    sweep_points = [1,2]
+    num_circuits = 1
+    num_qubits = 6
+    p = ql.Program('graycode6_47', num_qubits, platform)
+    p.set_sweep_points(sweep_points, num_circuits)
+    k = ql.Kernel('graycode6_47', platform)
+    k.gate('cnot',[1,0])
+    k.gate('cnot',[2,1])
+    k.gate('cnot',[3,2])
+    k.gate('cnot',[4,3])
+    k.gate('cnot',[5,4])
+
+    p.add_kernel(k)
+    p.compile()
+
