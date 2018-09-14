3643a61e8262928b8c7a64e99dbcb5d8a08ae4e2
Gonaco <danielmoremanza@gmail.com>
2018-09-14 13:53:22 +0200

    New translation

--- mapping_benchmarks/mod5d1_63.py
+++ mapping_benchmarks/mod5d1_63.py
@@ -2,70 +2,60 @@ from openql import openql as ql
 import os
 import argparse
 
-
-def circuit(config_file, scheduler='ASAP', output_dir_name='test_output', optimize='no', log_level='LOG_WARNING'):
+def circuit(config_file, scheduler='ASAP', mapper = 'base', output_dir_name='test_output', optimize='no', log_level='LOG_WARNING'):
     curdir = os.path.dirname(__file__)
     output_dir = os.path.join(curdir, output_dir_name)
     ql.set_option('output_dir', output_dir)
     ql.set_option('optimize', optimize)
     ql.set_option('scheduler', scheduler)
+    ql.set_option('mapper', mapper)
     ql.set_option('log_level', log_level)
 
     config_fn = os.path.join(curdir, config_file)
 
-    platform = ql.Platform('platform_none', config_fn)
-    sweep_points = [1, 2]
+    # platform  = ql.Platform('platform_none', config_fn)
+    platform  = ql.Platform('starmon', config_fn)
+    sweep_points = [1,2]
     num_circuits = 1
     num_qubits = 5
     p = ql.Program('mod5d1_63', platform, num_qubits)
     p.set_sweep_points(sweep_points, num_circuits)
     k = ql.Kernel('mod5d1_63', platform, num_qubits)
-    k.gate('cnot', [3, 1])
-    k.gate('cnot', [2, 0])
-    k.gate('cnot', [1, 4])
-    k.gate('cnot', [0, 4])
-    k.gate('h', [4])
-    k.gate('t', [1])
-    k.gate('t', [0])
-    k.gate('t', [4])
-    k.gate('cnot', [0, 1])
-    k.gate('cnot', [4, 0])
-    k.gate('cnot', [1, 4])
-    k.gate('tdag', [0])
-    k.gate('cnot', [1, 0])
-    k.gate('tdag', [1])
-    k.gate('tdag', [0])
-    k.gate('t', [4])
-    k.gate('cnot', [4, 0])
-    k.gate('cnot', [1, 4])
-    k.gate('cnot', [0, 1])
-    k.gate('h', [4])
-    k.gate('cnot', [3, 1])
-    k.gate('cnot', [2, 0])
+    k.gate('cnot',[3,1])
+    k.gate('cnot',[2,0])
+    k.gate('cnot',[1,4])
+    k.gate('cnot',[0,4])
+    k.gate('h',[4])
+    k.gate('t',[1])
+    k.gate('t',[0])
+    k.gate('t',[4])
+    k.gate('cnot',[0,1])
+    k.gate('cnot',[4,0])
+    k.gate('cnot',[1,4])
+    k.gate('tdag',[0])
+    k.gate('cnot',[1,0])
+    k.gate('tdag',[1])
+    k.gate('tdag',[0])
+    k.gate('t',[4])
+    k.gate('cnot',[4,0])
+    k.gate('cnot',[1,4])
+    k.gate('cnot',[0,1])
+    k.gate('h',[4])
+    k.gate('cnot',[3,1])
+    k.gate('cnot',[2,0])
 
     p.add_kernel(k)
-    p.compile()
-
+    p.compile()    ql.set_option('mapper', 'no')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='OpenQL compilation of a Quantum Algorithm')
-    parser.add_argument(
-        'config_file', help='Path to the OpenQL configuration file to compile this algorithm')
-    parser.add_argument(
-        '--scheduler', help='Scheduler specification (ASAP (default), ALAP, ...)')
-    parser.add_argument(
-        '--out_dir', help='Folder name to store the compilation')
+    parser = argparse.ArgumentParser(description='OpenQL compilation of a Quantum Algorithm')
+    parser.add_argument('config_file', help='Path to the OpenQL configuration file to compile this algorithm')
+    parser.add_argument('--scheduler', nargs='?', default='ASAP', help='Scheduler specification (ASAP (default), ALAP, ...)')
+    parser.add_argument('--mapper', nargs='?', default='base', help='Mapper specification (base, minextend, minextendrc)')
+    parser.add_argument('--out_dir', nargs='?', default='test_output', help='Folder name to store the compilation')
     args = parser.parse_args()
     try:
-        if args.out_dir and args.scheduler:
-            circuit(args.config_file, args.scheduler, args.out_dir)
-        elif args.scheduler:
-            circuit(args.config_file, args.scheduler)
-        elif args.out_dir:
-            circuit(args.config_file, out_dir_name=args.out_dir)
-        else:
-            circuit(args.config_file)
+        circuit(args.config_file, args.scheduler, args.mapper, args.out_dir)
     except TypeError:
         print('\nCompiled, but some gate is not defined in the configuration file. \nThe gate will be invoked like it is.')
         raise
