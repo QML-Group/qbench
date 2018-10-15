775abf19a6044acf7f5a95e5f4b6e2393053e03a
Gonaco <danielmoremanza@gmail.com>
2018-10-15 15:58:04 +0200

    Adapting the files that create the mapping folder

---
+++ benchmarks/Dani/cp_selected_benchmarks.sh
@@ -0,0 +1,44 @@
+cp 4gt11_82.py ../../mapping_benchmarks/                        
+cp 4gt12-v1_89.py ../../mapping_benchmarks/                     
+cp 4gt4-v0_72.py ../../mapping_benchmarks/                      
+cp 4mod5-bdd_287.py ../../mapping_benchmarks/                   
+cp 4mod5-v0_20.py ../../mapping_benchmarks/                     
+cp alu-bdd_288.py ../../mapping_benchmarks/                     
+cp alu-v0_27.py ../../mapping_benchmarks/                       
+cp benstein_vazirani_15b_secret_128.py ../../mapping_benchmarks/
+# cp cnt3-5_179.py ../../mapping_benchmarks/                      
+cp cuccaroAdder_1b.py ../../mapping_benchmarks/                 
+cp cuccaroMultiplier_1b.py ../../mapping_benchmarks/            
+cp decod24-bdd_294.py ../../mapping_benchmarks/                 
+cp decod24-enable_126.py ../../mapping_benchmarks/              
+cp graycode6_47.py ../../mapping_benchmarks/                    
+# cp ground_state_estimation_10.py ../../mapping_benchmarks/      
+cp ham3_102.py ../../mapping_benchmarks/                        
+cp hwb4_49.py ../../mapping_benchmarks/                         
+# cp ising_model_10.py ../../mapping_benchmarks/                  
+# cp life_238.py ../../mapping_benchmarks/                        
+cp miller_11.py ../../mapping_benchmarks/                       
+cp mini-alu_167.py ../../mapping_benchmarks/                    
+# cp mini_alu_305.py ../../mapping_benchmarks/                    
+cp mod10_176.py ../../mapping_benchmarks/                       
+cp mod5adder_127.py ../../mapping_benchmarks/                   
+cp mod5d1_63.py ../../mapping_benchmarks/                       
+cp mod8-10_177.py ../../mapping_benchmarks/                     
+cp one-two-three-v1_99.py ../../mapping_benchmarks/             
+cp one-two-three-v3_101.py ../../mapping_benchmarks/            
+# cp plus63mod4096_163.py ../../mapping_benchmarks/               
+# cp qft_10.py ../../mapping_benchmarks/                          
+cp rd32-v0_66.py ../../mapping_benchmarks/                      
+cp sf_274.py ../../mapping_benchmarks/                          
+cp sf_276.py ../../mapping_benchmarks/                          
+cp sqrt8_260.py ../../mapping_benchmarks/                       
+# cp squar5_261.py ../../mapping_benchmarks/                      
+# cp square_root_7.py ../../mapping_benchmarks/                   
+cp sym6_145.py ../../mapping_benchmarks/                        
+# cp sym6_316.py ../../mapping_benchmarks/                        
+# cp urf2_152.py ../../mapping_benchmarks/                        
+# cp urf2_277.py ../../mapping_benchmarks/                        
+cp vbeAdder_2b.py ../../mapping_benchmarks/                     
+cp xor5_254.py ../../mapping_benchmarks/                        
+cp shor_15.py ../../mapping_benchmarks/
+cp grover_orcl_toff.py ../../mapping_benchmarks/
