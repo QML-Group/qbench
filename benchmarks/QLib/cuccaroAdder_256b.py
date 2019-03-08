bbb15a644eb3fbc70f4220bfcaa0e7616293e949
Gonaco <danielmoremanza@gmail.com>
2019-03-08 10:15:43 +0100

    New translation

--- benchmarks/QLib/cuccaroAdder_256b.py
+++ benchmarks/QLib/cuccaroAdder_256b.py
@@ -2,7 +2,7 @@ from openql import openql as ql
 import os
 import argparse
 
-def circuit(config_file, scheduler='ASAP', uniform_sched= 'no', mapper='base', initial_placement='no', output_dir_name='test_output', optimize='no', measurement=True, log_level='LOG_WARNING'):
+def circuit(config_file, new_scheduler='yes', scheduler='ASAP', uniform_sched= 'no', sched_commute = 'yes', mapper='base', moves='no', maptibreak='random', initial_placement='no', output_dir_name='test_output', optimize='no', measurement=True, log_level='LOG_WARNING'):
     curdir = os.path.dirname(__file__)
     output_dir = os.path.join(curdir, output_dir_name)
     ql.set_option('output_dir', output_dir)
@@ -12,6 +12,10 @@ def circuit(config_file, scheduler='ASAP', uniform_sched= 'no', mapper='base', i
     ql.set_option('mapper', mapper)
     ql.set_option('initialplace', initial_placement)
     ql.set_option('log_level', log_level)
+    ql.set_option('scheduler_post179', new_scheduler)
+    ql.set_option('scheduler_commute', sched_commute)
+    ql.set_option('mapusemoves', moves)
+    ql.set_option('maptiebreak', maptiebreak)
 
     config_fn = os.path.join(curdir, config_file)
 
@@ -1572,9 +1576,13 @@ def circuit(config_file, scheduler='ASAP', uniform_sched= 'no', mapper='base', i
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OpenQL compilation of a Quantum Algorithm')
     parser.add_argument('config_file', help='Path to the OpenQL configuration file to compile this algorithm')
+    parser.add_argument('--new_scheduler', nargs='?', default='yes', help='Scheduler defined by Hans')
     parser.add_argument('--scheduler', nargs='?', default='ASAP', help='Scheduler specification (ASAP (default), ALAP, ...)')
     parser.add_argument('--uniform_sched', nargs='?', default='no', help='Uniform shceduler actication (yes or no)')
+    parser.add_argument('--sched_commute', nargs='?', default='yes', help='Permits two-qubit gates to be commutable')
     parser.add_argument('--mapper', nargs='?', default='base', help='Mapper specification (base, minextend, minextendrc)')
+    parser.add_argument('--moves', nargs='?', default='no', help='Let the use of moves')
+    parser.add_argument('--maptiebreak', nargs='?', default='random', help=''
     parser.add_argument('--initial_placement', nargs='?', default='no', help='Initial placement specification (yes or no)')
     parser.add_argument('--out_dir', nargs='?', default='test_output', help='Folder name to store the compilation')
     parser.add_argument('--measurement', nargs='?', default=True, help='Add measurement to all the qubits in the end of the algorithm')
