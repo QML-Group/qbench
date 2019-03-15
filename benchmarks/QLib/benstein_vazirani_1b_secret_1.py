1f831854fb3b56f031cf81cd7b5c6651f7b9c206
Gonaco <danielmoremanza@gmail.com>
2019-03-15 18:57:32 +0100

    New translation

--- benchmarks/QLib/benstein_vazirani_1b_secret_1.py
+++ benchmarks/QLib/benstein_vazirani_1b_secret_1.py
@@ -2,12 +2,13 @@ from openql import openql as ql
 import os
 import argparse
 
-def circuit(config_file, scheduler='ASAP', mapper='base', initial_placement='no', output_dir_name='test_output', optimize='no', log_level='LOG_WARNING'):
+def circuit(config_file, scheduler='ASAP', uniform_sched= 'no', mapper='base', initial_placement='no', output_dir_name='test_output', optimize='no', measurement=True, log_level='LOG_WARNING'):
     curdir = os.path.dirname(__file__)
     output_dir = os.path.join(curdir, output_dir_name)
     ql.set_option('output_dir', output_dir)
     ql.set_option('optimize', optimize)
     ql.set_option('scheduler', scheduler)
+    ql.set_option('scheduler_uniform', uniform_sched)
     ql.set_option('mapper', mapper)
     ql.set_option('initialplace', initial_placement)
     ql.set_option('log_level', log_level)
@@ -30,6 +31,10 @@ def circuit(config_file, scheduler='ASAP', mapper='base', initial_placement='no'
     k.gate('h',[0])
     k.gate('h',[1])
 
+    if measurement:
+        for q in range(num_qubits):
+            k.gate('measure', [q])
+
     p.add_kernel(k)
     p.compile()
     ql.set_option('mapper', 'no')
@@ -38,9 +43,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OpenQL compilation of a Quantum Algorithm')
     parser.add_argument('config_file', help='Path to the OpenQL configuration file to compile this algorithm')
     parser.add_argument('--scheduler', nargs='?', default='ASAP', help='Scheduler specification (ASAP (default), ALAP, ...)')
+    parser.add_argument('--uniform_sched', nargs='?', default='no', help='Uniform shceduler actication (yes or no)')
     parser.add_argument('--mapper', nargs='?', default='base', help='Mapper specification (base, minextend, minextendrc)')
     parser.add_argument('--initial_placement', nargs='?', default='no', help='Initial placement specification (yes or no)')
     parser.add_argument('--out_dir', nargs='?', default='test_output', help='Folder name to store the compilation')
+    parser.add_argument('--measurement', nargs='?', default=True, help='Add measurement to all the qubits in the end of the algorithm')
     args = parser.parse_args()
     try:
         circuit(args.config_file, args.scheduler, args.mapper, args.initial_placement, args.out_dir)
